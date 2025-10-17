9f96ff3342e0eb36ab38f3950d6d213cf4da8958
AustinW787 <aaw3982@g.rit.edu>
2025-10-16 20:36:30 -0400

    Finalized Rules
    Used the manual to fix rules

--- Assets/Scripts/Gameplay/Overhead/Package.cs
+++ Assets/Scripts/Gameplay/Overhead/Package.cs
@@ -1,5 +1,5 @@
 using System;
 using UnityEngine;
 
-public record Package(bool Valid, Vector2 WeightPair, string From, string ShipTo, string Date, string Remark, int ID, string Shipper, 
+public record Package(bool Valid, Vector2 WeightPair, string From, string ShipTo, string Date, string Remark, string ID, string Shipper,
     Action<GameObject, bool> OnProcessedCallback = null, Action<GameObject> OnSpawnedCallback = null);
